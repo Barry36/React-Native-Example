a356b5e844d463d0939d8789206646e9d199c2e9
Barry Wang <j795wang@uwaterloo.ca>
2021-01-17 19:04:54 -0500

    1

---
+++ react-native-web-workout-series/src/App.js
@@ -0,0 +1,17 @@
+import { View } from 'react-native';
+import './App.css';
+
+
+const App = App.FC = () => {
+  return (
+    <View>
+      <Text>
+        hello from react native web
+      </Text>
+
+      {/* <Text></Text> */}
+    </View>
+  );
+}
+
+export default App;
